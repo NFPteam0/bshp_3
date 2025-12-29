2c4d7b9baad44568ccb14b210c8d2c2f4f89f00e
Софья Кравченко <svkravchenko@1cbit.ru>
2025-12-30 02:17:43 +0300

    .

--- bshp_ml/app/tasks/reader.py
+++ bshp_ml/app/tasks/reader.py
@@ -21,9 +21,9 @@ class Reader:
     async def read(self, data_filter) -> pd.DataFrame:
         if USE_DETAILED_LOG:
             logger.info("Start reading data")
-        data = await db_processor.find(
-            "raw_data", convert_dates_in_db_filter(data_filter), batch_size=1000
-        )
+        # почему? data = await db_processor.find(
+        #     "raw_data", convert_dates_in_db_filter(data_filter), batch_size=1000
+        data = await db_processor.find("raw_data", data_filter, batch_size=1000)
         pd_data = pd.DataFrame(data)
 
         if USE_DETAILED_LOG:

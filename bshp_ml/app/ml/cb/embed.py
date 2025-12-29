4d113d0906ca4b4c145ae9d55c20394796898eeb
Софья Кравченко <svkravchenko@1cbit.ru>
2025-12-29 20:56:23 +0300

    .

--- bshp_ml/app/ml/cb/embed.py
+++ bshp_ml/app/ml/cb/embed.py
@@ -957,7 +957,7 @@ class CatBoostModelEmbeddings(CatBoostModel):
         self.classes = {}
 
         for y in self.y_columns:
-            dataset = dataset[y].replace("", -1)
+            dataset[y] = dataset[y].replace("", -1)
             if y == "cash_flow_details_code":
                 self.classes[y] = {}
                 for item in dataset["cash_flow_item_code"].unique():
